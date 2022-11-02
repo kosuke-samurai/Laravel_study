bb04f01976445a50db7a242a7ad96ae9046b6c87
kosuke-samurai <103375028+kosuke-samurai@users.noreply.github.com>
2022-11-02 16:28:45 +0900

    復習用

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\UtilityController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*
+Route::get('/', function () {
+    return view('welcome');
+});
+*/
+
+Route::get('/hello_world', fn () => view('hello_world'));
+
+Route::get('/hello', fn () => view('hello', [
+    'name' => '山田',
+    'course' => 'Laravel9'
+]));
+
+Route::get('/', fn () => view('index'));
+Route::get('/curriculum', fn () => view('curriculum'));
+
+
+// 世界の時間
+Route::get('/world-time', [UtilityController::class, 'worldtime']);
+
+// おみくじ
+Route::get('/omikuji', [GameController::class, 'omikuji']);
+
+// モンティ・ホール問題
+Route::get('/monty-hall', [GameController::class, 'montyhall']);
